257e26ba528f2147f8ff15ef50c84a81558dad07
Gilbert <gilbert@Gilberts-MBP-2.fios-router.home>
2016-12-07 20:37:04 -0500

    [Fix] ADDI Only Support 32-bit Immediates

--- init.asm
+++ init.asm
@@ -1,85 +1,161 @@
 -- Init S arrary -- 
-ADDI $15, xb7e15163
+ADDI $15, $15, xb7e1
+SHL $15, $15, 4
+ADDI $15, $15, x5163
 SW $15, 0($0)
-ADDI $15, x5618cb1c
+ADDI $15, $15, x5618
+SHL $15, $15, 4
+ADDI $15, $15, xcb1c
 SW $15, 1($0)
-ADDI $15, xf45044d5
+ADDI $15, $15, xf450
+SHL $15, $15, 4
+ADDI $15, $15, x44d5
 SW $15, 2($0)
-ADDI $15, x9287be8e
+ADDI $15, $15, x9287
+SHL $15, $15, 4
+ADDI $15, $15, xbe8e
 SW $15, 3($0)
-ADDI $15, x30bf3847
+ADDI $15, $15, x30bf
+SHL $15, $15, 4
+ADDI $15, $15, x3847
 SW $15, 4($0)
-ADDI $15, xcef6b200
+ADDI $15, $15, xcef6
+SHL $15, $15, 4
+ADDI $15, $15, xb200
 SW $15, 5($0)
-ADDI $15, x6d2e2bb9
+ADDI $15, $15, x6d2e
+SHL $15, $15, 4
+ADDI $15, $15, x2bb9
 SW $15, 6($0)
-ADDI $15, x0b65a572
+ADDI $15, $15, x0b65
+SHL $15, $15, 4
+ADDI $15, $15, xa572
 SW $15, 7($0)
-ADDI $15, xa99d1f2b
+ADDI $15, $15, xa99d
+SHL $15, $15, 4
+ADDI $15, $15, x1f2b
 SW $15, 8($0)
-ADDI $15, x47d498e4
+ADDI $15, $15, x47d4
+SHL $15, $15, 4
+ADDI $15, $15, x98e4
 SW $15, 9($0)
-ADDI $15, xe60c129d
+ADDI $15, $15, xe60c
+SHL $15, $15, 4
+ADDI $15, $15, x129d
 SW $15, 10($0)
-ADDI $15, x84438c56
+ADDI $15, $15, x8443
+SHL $15, $15, 4
+ADDI $15, $15, x8c56
 SW $15, 11($0)
-ADDI $15, x227b060f
+ADDI $15, $15, x227b
+SHL $15, $15, 4
+ADDI $15, $15, x060f
 SW $15, 12($0)
-ADDI $15, xc0b27fc8
+ADDI $15, $15, xc0b2
+SHL $15, $15, 4
+ADDI $15, $15, x7fc8
 SW $15, 13($0)
-ADDI $15, x5ee9f981
+ADDI $15, $15, x5ee9
+SHL $15, $15, 4
+ADDI $15, $15, xf981
 SW $15, 14($0)
-ADDI $15, xfd21733a
+ADDI $15, $15, xfd21
+SHL $15, $15, 4
+ADDI $15, $15, x733a
 SW $15, 15($0)
-ADDI $15, x9b58ecf3
+ADDI $15, $15, x9b58
+SHL $15, $15, 4
+ADDI $15, $15, xecf3
 SW $15, 16($0)
-ADDI $15, x399066ac
+ADDI $15, $15, x3990
+SHL $15, $15, 4
+ADDI $15, $15, x66ac
 SW $15, 17($0)
-ADDI $15, xd7c7e065
+ADDI $15, $15, xd7c7
+SHL $15, $15, 4
+ADDI $15, $15, xe065
 SW $15, 18($0)
-ADDI $15, x75ff5a1e
+ADDI $15, $15, x75ff
+SHL $15, $15, 4
+ADDI $15, $15, x5a1e
 SW $15, 19($0)
-ADDI $15, x1436d3d7
+ADDI $15, $15, x1436
+SHL $15, $15, 4
+ADDI $15, $15, xd3d7
 SW $15, 20($0)
-ADDI $15, xb26e4d90
+ADDI $15, $15, xb26e
+SHL $15, $15, 4
+ADDI $15, $15, x4d90
 SW $15, 21($0)
-ADDI $15, x50a5c749
+ADDI $15, $15, x50a5
+SHL $15, $15, 4
+ADDI $15, $15, xc749
 SW $15, 22($0)
-ADDI $15, xeedd4102
+ADDI $15, $15, xeedd
+SHL $15, $15, 4
+ADDI $15, $15, x4102
 SW $15, 23($0)
-ADDI $15, x8d14babb
+ADDI $15, $15, x8d14
+SHL $15, $15, 4
+ADDI $15, $15, xbabb
 SW $15, 24($0)
-ADDI $15, x2b4c3474
+ADDI $15, $15, x2b4c
+SHL $15, $15, 4
+ADDI $15, $15, x3474
 SW $15, 25($0)
 
 -- Init L array --
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 26($0)
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 27($0)
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 28($0)
-ADDI $15, x00000001
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0001
 SW $15, 29($0)
 
 -- Init user key -- 
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 30($0)
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 31($0)
-ADDI $15, x00000000
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0000
 SW $15, 32($0)
-ADDI $15, x00000001
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0001
 SW $15, 33($0)
 
 -- Init Pw and Qw -- 
-ADDI $15, xB7E15163
+ADDI $15, $15, xB7E1
+SHL $15, $15, 4
+ADDI $15, $15, x5163
 SW $15, 0($0)
-ADDI $15, x9E3779B9
+ADDI $15, $15, x9E37
+SHL $15, $15, 4
+ADDI $15, $15, x79B9
 SW $15, 0($0)
 
 -- Init AB -- 
-ADDI $15, x00000001
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0001
 SW $15, 0($0)
-ADDI $15, x00000001
+ADDI $15, $15, x0000
+SHL $15, $15, 4
+ADDI $15, $15, x0001
 SW $15, 0($0)
