3c1dd50290e55284843eb39577e90d8a4eda55a1
Gilbert <gilbert@172-16-27-116.DYNAPOOL.NYU.EDU>
2016-12-13 13:29:38 -0500

    Fix XOR

--- asm/encrypt.asm
+++ asm/encrypt.asm
@@ -1,25 +1,20 @@
--- Encrypt --
 ADDI $30, $0, 32
 ADDI $12, $0, 1 // init iterator
 ADDI $13, $0, 13
-
 LW $1, 36($0) // load a from MEM[36]
 LW $2, 37($0) // load b from MEM[37]
 LW $9, 0($0) // load s[0]
 LW $10, 1($0) // load s[1]
 ADD $1, $1, $9 // a = a + s[0]
 ADD $2, $2, $10 // b = b + s[1]
-
--- Loop Start --
 ADD $11, $12, $12 // 2 * index
 LW $9, 0($11) // load s[2*index]
 LW $10, 1($11) // load s[2*index+1]
-
 NOR $3, $1, $1 
 NOR $4, $2, $2 
-OR $3, $3, $2 
-OR $4, $4, $1 
-AND $1, $3, $4 // a xor b = (!a or b) and (a or !b)
+AND $3, $3, $2 
+AND $4, $4, $1 
+OR $1, $3, $4 // a xor b = (!a or b) and (a or !b)
 ADD $3, $0, $1
 ADD $4, $0, $2
 ANDI $4, $4, 31
@@ -35,12 +30,11 @@ ADDI $4, $4, 1
 BNE $4, $30, -3 // shift a left by 32-b bits
 OR $1, $1, $3 // left rotate a by b bits
 ADD $1, $1, $9 
-
 NOR $3, $1, $1
 NOR $4, $2, $2 
-OR $3, $3, $2 
-OR $4, $4, $1 
-AND $2, $3, $4 // a xor b = (!a or b) and (a or !b)
+AND $3, $3, $2 
+AND $4, $4, $1 
+OR $2, $3, $4 // a xor b = (!a or b) and (a or !b)
 ADD $3, $0, $2
 ADD $4, $0, $1
 ANDI $4, $4, 31
@@ -56,10 +50,7 @@ ADDI $4, $4, 1
 BNE $4, $30, -3 // shift b left by 32-a bits
 OR $2, $2, $3 // left rotate b by a bits
 ADD $2, $2, $10
-
 ADDI $12, $12, 1 // increment index by 1
-BNE $12, $13, -41 // loop 12 times
--- Loop End --
-
+BNE $12, $13, -45 // loop 12 times
 SW $1, 38($0) // Store encrypted a to MEM[38]
 SW $2, 39($0) // Store encrypted b to MEM[39]
